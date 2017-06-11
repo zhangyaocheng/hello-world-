39c8095c74230ce04829ff8382b6ac0b47f97af7
zhangyaocheng <910486122@qq.com>
2017-06-11 16:26:15 +0800

    构建了一个人对象

---
+++ MyProIdea/People.py
@@ -0,0 +1,18 @@
+#coding:utf-8
+
+class People(object):
+
+    def __init__(self,sex):
+        self.sex = sex
+        
+    def say(self,):
+        pass
+    def walk(self,):
+        pass
+    
+    def breath(self,):
+        pass
+    
+    def getsex(self,):
+        return self.sex
+    
